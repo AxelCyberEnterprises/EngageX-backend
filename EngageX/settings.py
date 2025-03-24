a383cac73dc2b725f114ab18c5ad1e6cd94af900
Alabi Khalid <alabikhalid.12@gmail.com>
2025-03-24 22:25:28 +0100

    updated cors-header

--- EngageX/settings.py
+++ EngageX/settings.py
@@ -22,120 +22,115 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wfl*mho^tyaghxhwx4p^2u8)yl#gw+^ub&(=!m#=!x3rrqo1og'
+SECRET_KEY = "django-insecure-wfl*mho^tyaghxhwx4p^2u8)yl#gw+^ub&(=!m#=!x3rrqo1og"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.elasticbeanstalk.com', '*']
+ALLOWED_HOSTS = [".elasticbeanstalk.com", "*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # Apps I installed
-    'rest_framework',
-    'corsheaders',
-    'users',
-    'payments',
-    'practice_sessions',
-    'streaming',
-
+    "rest_framework",
+    "corsheaders",
+    "users",
+    "payments",
+    "practice_sessions",
+    "streaming",
     # Apps for authentication
-    'djoser',
-    'rest_framework.authtoken',
-
+    "djoser",
+    "rest_framework.authtoken",
     # API Documentation
-    'drf_yasg',
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    #Middleware I added
-    'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # Middleware I added
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 
 # REST Framework settings
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_PARSER_CLASSES": (
+        "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.FormParser",
+        "rest_framework.parsers.MultiPartParser",
     ),
-    'DEFAULT_PARSER_CLASSES': (
-        'rest_framework.parsers.JSONParser',
-        'rest_framework.parsers.FormParser',
-        'rest_framework.parsers.MultiPartParser',
-    ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
 }
 
 DJOSER = {
-    'LOGIN_FIELD': 'email',
-    'USER_CREATE_PASSWORD_RETYPE': True,
-    'SEND_ACTIVATION_EMAIL': True,  # Make sure this is enabled
-    'SEND_CONFIRMATION_EMAIL': False,
-    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
-    'ACTIVATION_URL': 'activate/{uid}/{token}',  # Activation link URL
-    'SERIALIZERS': {
-        'user_create': 'users.serializers.UserSerializer',
-        'user': 'users.serializers.UserSerializer',
-        'token_create': 'users.serializers.CustomTokenCreateSerializer',
+    "LOGIN_FIELD": "email",
+    "USER_CREATE_PASSWORD_RETYPE": True,
+    "SEND_ACTIVATION_EMAIL": True,  # Make sure this is enabled
+    "SEND_CONFIRMATION_EMAIL": False,
+    "PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
+    "ACTIVATION_URL": "activate/{uid}/{token}",  # Activation link URL
+    "SERIALIZERS": {
+        "user_create": "users.serializers.UserSerializer",
+        "user": "users.serializers.UserSerializer",
+        "token_create": "users.serializers.CustomTokenCreateSerializer",
     },
 }
 
 
-AUTH_USER_MODEL = 'users.CustomUser'
+AUTH_USER_MODEL = "users.CustomUser"
 
 
-ROOT_URLCONF = 'EngageX.urls'
+ROOT_URLCONF = "EngageX.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'EngageX.wsgi.application'
+WSGI_APPLICATION = "EngageX.wsgi.application"
 
-ASGI_APPLICATION = 'EngageX.asgi.application'
+ASGI_APPLICATION = "EngageX.asgi.application"
 
 # Socket.IO settings
 SOCKETIO = {
-    'CORS_ALLOWED_ORIGINS': '*',  # Configure this appropriately in production
-    'ASYNC_MODE': 'asgi',
+    "CORS_ALLOWED_ORIGINS": "*",  # Configure this appropriately in production
+    "ASYNC_MODE": "asgi",
 }
 CORS_ALLOWED_ORIGINS = [
     "https://www.engagexai.io",
     "http://localhost:5173",
+    "https://main.d2wwdi7x8g70xe.amplifyapp.com",
 ]
 
 # Database
@@ -163,37 +158,36 @@ CORS_ALLOWED_ORIGINS = [
 #     }
 # }
 
-if 'RDS_HOSTNAME' in os.environ:
+if "RDS_HOSTNAME" in os.environ:
     # Production settings (AWS RDS MySQL)
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['POSTGRESQL_DATABASE_NAME'],
-            'USER': os.environ['POSTGRESQL_USERNAME'],
-            'PASSWORD': os.environ['POSTGRESQL_PASSWORD'],
-            'HOST': os.environ['POSTGRESQL_SERVER_NAME'],
-            'PORT': os.environ.get('POSTGRESS_PORT', default='5432'),
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ["POSTGRESQL_DATABASE_NAME"],
+            "USER": os.environ["POSTGRESQL_USERNAME"],
+            "PASSWORD": os.environ["POSTGRESQL_PASSWORD"],
+            "HOST": os.environ["POSTGRESQL_SERVER_NAME"],
+            "PORT": os.environ.get("POSTGRESS_PORT", default="5432"),
         }
     }
-    OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+    OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 else:
     # Local development settings (PostgreSQL)
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('POSTGRESQL_DATABASE_NAME'),
-            'USER': config('POSTGRESQL_USERNAME'),
-            'PASSWORD': config('POSTGRESQL_PASSWORD'),
-            'HOST': config('POSTGRESQL_SERVER_NAME'),
-            'PORT': config('PORT', default='5432'),
-            'OPTIONS': {
-                'sslmode': 'require',
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": config("POSTGRESQL_DATABASE_NAME"),
+            "USER": config("POSTGRESQL_USERNAME"),
+            "PASSWORD": config("POSTGRESQL_PASSWORD"),
+            "HOST": config("POSTGRESQL_SERVER_NAME"),
+            "PORT": config("PORT", default="5432"),
+            "OPTIONS": {
+                "sslmode": "require",
             },
         }
     }
 
-    OPENAI_API_KEY = config('OPENAI_API_KEY')
-
+    OPENAI_API_KEY = config("OPENAI_API_KEY")
 
 
 # Password validation
@@ -201,30 +195,30 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',  # Default authentication
+    "django.contrib.auth.backends.ModelBackend",  # Default authentication
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -234,43 +228,43 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # settings.py
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
         },
-        'file': {
-            'level': 'ERROR',
-            'class': 'logging.FileHandler',
-            'filename': 'error.log',
+        "file": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "filename": "error.log",
         },
     },
-    'loggers': {
-        'streaming.consumers': {
-            'handlers': ['console', 'file'],
-            'level': 'ERROR',
-            'propagate': False,
+    "loggers": {
+        "streaming.consumers": {
+            "handlers": ["console", "file"],
+            "level": "ERROR",
+            "propagate": False,
         },
         # Add other loggers as needed
     },
-    'root': {
-        'handlers': ['console'],
-        'level': 'INFO',
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
     },
 }
