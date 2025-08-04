2720b9a3f22cc8d3c0913f86fd1c0b4a2220c424
khidrhussein <husseinkhidr3@gmail.com>
2025-08-04 13:19:07 +0100

    bug fix: added missing import

--- enterprise/views.py
+++ enterprise/views.py
@@ -140,7 +140,7 @@ import uuid
 import boto3
 import traceback
 import logging
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.decorators import action
